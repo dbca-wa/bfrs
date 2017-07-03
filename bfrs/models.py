13198cd8170b59496126715b319c991243847345
ASI <asi@dpaw.wa.gov.au>
2017-07-03 10:24:01 +0800

    updated Lat/Lon output format

--- bfrs/models.py
+++ bfrs/models.py
@@ -442,7 +442,12 @@ class Bushfire(Audit):
         # need to format float number (seconds) to 1 dp
         lon[2] = str(round(eval(lon[2]), 1))
         lat[2] = str(round(eval(lat[2]), 1))
-        return '(Deg/Min/Sec) ' + str(' '.join(lon)) + ', ' + str(' '.join(lat))
+
+        # Degrees Minutes Seconds Hemisphere
+        lat_str = lat[0] + u'\N{DEGREE SIGN} ' + lat[1].zfill(2) + '\' ' + lat[2].zfill(4) + '\" ' + lat[3]
+        lon_str = lon[0] + u'\N{DEGREE SIGN} ' + lon[1].zfill(2) + '\' ' + lon[2].zfill(4) + '\" ' + lon[3]
+
+        return 'Lat/Lon: ' + lat_str + ', ' + lon_str
 
     @property
     def time_to_control_hours_part(self):
