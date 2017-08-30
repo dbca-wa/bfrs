a669d05a6afcfc6b92a40ab7eff63dbf63be686d
ASI <asi@dpaw.wa.gov.au>
2017-08-30 12:22:44 +0800

    separated drop and create views in mgt cmds

--- bfrs/management/commands/drop_views.py
+++ bfrs/management/commands/drop_views.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from optparse import make_option
 from django.conf import settings
-from bfrs.sql_views import create_view, create_final_view, create_fireboundary_view
+from bfrs.sql_views import drop_view, drop_final_view, drop_fireboundary_view
 
 import os
 import sys
@@ -10,14 +10,14 @@ import logging
 logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
-    help = 'Creates sql views \n \
+    help = 'Drops sql views \n \
 \n \
-        usage: ./manage.py create_views \n \
+        usage: ./manage.py drop_views \n \
     '
 
     def handle(self, *args, **options):
-        create_view()
-        create_final_view()
-        create_fireboundary_view()
+        drop_view()
+        drop_final_view()
+        drop_fireboundary_view()
         self.stdout.write('Done')
 
