601b20f146d6f4a66a94509fe1b32773661f5156
Jason Moore <jason.moore@dbca.wa.gov.au>
2024-08-02 14:41:33 +0800

    openssl fix and docker cleanup

--- startup.sh
+++ startup.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-eval $(grep -v '^#' /container-config/.cronenv | xargs -d "\n" -I {} echo export \"{}\" )
-env
-whoami
-# how to create a new password.
-# echo test1test1 | openssl passwd -1 -stdin
-if [ -z "$SUDO_OIM_ENCRYPTED_PASSWORD" ]; then
-        echo "SUDO_OIM_ENCRYPTED_PASSWORD Ignored":
-else
-        echo "SUDO_OIM_ENCRYPTED_PASSWORD Preparing";
-        SUDO_OIM_ENCRYPTED_PASSWORD_SIZE="$(echo $SUDO_OIM_ENCRYPTED_PASSWORD | wc -m)"
-        if [ "$SUDO_OIM_ENCRYPTED_PASSWORD_SIZE" -gt "10" ]; then
-        usermod -p "$SUDO_OIM_ENCRYPTED_PASSWORD" oim
-        echo "SUDO_OIM_ENCRYPTED_PASSWORD Updated";
-        fi
+# Start the first process
+cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
+
+if [ $ENABLE_CRON == "True" ];
+then
+echo "Starting Python Cron"
+python /bin/scheduler.py /app/python-cron /app/logs/python-cron.log &
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start cron: $status"
+  exit $status
 fi
 
-if [ $ENABLE_CRON == "True" ]; then
-        echo "Starting Cron"
-        service cron start &
-        status=$?
-        if [ $status -ne 0 ]; then
-        echo "Failed to start cron: $status"
-        exit $status
-        fi
 fi
+
+if [ $ENABLE_WEB == "True" ];
+    then
+echo "Starting Gunicorn"
+# Start the second process
+
+/app/venv/bin/gunicorn  bfrs_project.wsgi --bind :8080 --config /app/gunicorn.ini
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start gunicorn: $status"
+  exit $status
+fi
+else
+   echo "ENABLE_WEB environment vairable not set to True, web server is not starting."
+   /bin/bash
+fi
+
