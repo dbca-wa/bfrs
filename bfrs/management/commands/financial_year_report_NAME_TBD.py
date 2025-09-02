5f79c429f3f3083412988ea0d6744bed46308ef4
Karsten Prehn <karsten.prehn@gmail.com>
2025-09-02 04:33:56 +0000

    Added a financial year report django management command python script

---
+++ bfrs/management/commands/financial_year_report_NAME_TBD.py
@@ -0,0 +1,196 @@
+from io import BytesIO
+
+from django.db import connection, transaction
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.core.management.base import BaseCommand
+from django.template.loader import render_to_string
+from django.utils import timezone
+
+from datetime import datetime
+from email.mime.application import MIMEApplication
+import pandas as pd
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = "Generate a financial year report"
+
+    FILENAME = "financial_year_report"
+    DISCREPANCY_SHEET_NAME = "Discrepancy Report"
+    ALL_DATA_SHEET_NAME = "All Data"
+    EMAIL_SUBJECT = "Financial Year Report for {financial_year}"
+    SHEET_DISCREPANCY_SQL = """
+        SELECT
+                *,
+                spatial_geometry_total_area - bfrs_area_yellow AS diff_bfrs_area_yellow,
+                spatial_geometry_total_area - reporting_area_grey AS diff_reporting_area_grey
+        FROM (
+                SELECT
+                        id,
+                        fire_number,
+                        ROUND((ST_Area(ST_Transform(fire_boundary, 900914)) / 10000)::numeric, 2) AS spatial_geometry_total_area,
+        (
+                                SELECT
+                                        sum(area)
+                                FROM
+                                        bfrs_areaburnt
+                                WHERE
+                                        bushfire_id = bf.id) AS bfrs_area_yellow,
+        (
+                                        SELECT
+                                                sum(area)
+                                        FROM
+                                                reporting_areaburnt
+                                        WHERE
+                                                bushfire_id = bf.id) AS reporting_area_grey
+                                FROM
+                                        bfrs_bushfire AS bf
+                                WHERE
+                                        bf.reporting_year = %s
+                                        AND bf.report_status IN (3, 4)) AS bfrs_spatial_compare
+        WHERE
+                spatial_geometry_total_area != bfrs_area_yellow
+                OR spatial_geometry_total_area != reporting_area_grey;
+    """
+    SHEET_ALL_DATA_SQL = """
+        SELECT
+                id,
+                fire_number,
+                ROUND((ST_Area(ST_Transform(fire_boundary, 900914)) / 10000)::numeric, 2) AS spatial_geometry_total_area,
+        (
+                        SELECT
+                                sum(area)
+                        FROM
+                                bfrs_areaburnt
+                        WHERE
+                                bushfire_id = bf.id) AS bfrs_area_yellow,
+        (
+                        SELECT
+                                sum(area)
+                        FROM
+                                reporting_areaburnt
+                        WHERE
+                                bushfire_id = bf.id) AS reporting_area_grey
+        FROM
+                bfrs_bushfire AS bf
+        WHERE
+                bf.reporting_year = %s
+                AND bf.report_status IN (3, 4);
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--dry-run",
+            action="store_true",
+            help="Run the command in dry run mode.",
+        )
+        parser.add_argument(
+            "--financial-year",
+            type=int,
+            default=timezone.now().year,
+            help="Specify the financial year for the report. If not provided, the current year will be used.",
+        )
+        return super().add_arguments(parser)
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        logger.info("Generating financial year report...")
+
+        dry_run = options["dry_run"]
+        financial_year = options.get("financial_year", self.get_financial_year())
+        params = [financial_year]
+
+        # Execute SQL queries
+        with connection.cursor() as cursor:
+            cursor.execute(self.SHEET_DISCREPANCY_SQL, params)
+            rows_discrepancy = cursor.fetchall()
+            # Get discrepancy column names
+            columns_discrepancy = [col[0] for col in cursor.description]
+
+            cursor.execute(self.SHEET_ALL_DATA_SQL, params)
+            rows_all_data = cursor.fetchall()
+            # Get all data column names
+            columns_all_data = [col[0] for col in cursor.description]
+
+        # Convert rows to pandas DataFrames
+        df_discrepancy = pd.DataFrame(rows_discrepancy, columns=columns_discrepancy)
+        df_all_data = pd.DataFrame(rows_all_data, columns=columns_all_data)
+
+        if dry_run:
+            logger.info("Dry run mode is enabled. No changes will be made.")
+
+            logger.info(f"Discrepancy report for financial year {financial_year}:")
+            logger.info(df_discrepancy)
+            logger.info(f"All data report for financial year {financial_year}:")
+            logger.info(df_all_data)
+        else:
+            filename = f"{self.FILENAME}-{financial_year}.xlsx"
+            output = BytesIO()
+            # Create Excel file
+            with pd.ExcelWriter(output, engine="openpyxl") as writer:
+                df_discrepancy.to_excel(
+                    writer, sheet_name=self.DISCREPANCY_SHEET_NAME, index=False
+                )
+                df_all_data.to_excel(writer, sheet_name=self.ALL_DATA_SHEET_NAME, index=False)
+            output.seek(0)  # Reset the pointer to the beginning of the BytesIO object
+            file = MIMEApplication(output.read(), name=filename)
+            file["Content-Disposition"] = f'attachment; filename="{filename}"'
+
+            # Send report via email
+            self.send_notification_email(
+                subject=self.EMAIL_SUBJECT.format(financial_year=financial_year),
+                attachments=[file],
+            )
+            logger.info("Financial year report generated successfully.")
+
+        # Done
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"\nReport for financial year {financial_year} generated successfully."
+            )
+        )
+
+    def get_financial_year(self):
+        """
+        Get the current financial year based on the current date.
+        """
+
+        today = datetime.now()
+        if today.month >= 7:  # From July to December
+            return today.year
+        else:  # From January to June
+            return today.year - 1
+
+    def send_notification_email(self, subject="Financial Year Report", attachments=[]):
+        template = "bfrs/email/financial_year_report.html"
+        user_email = "karsten.prehn@dbca.wa.gov.au"
+        to_email = settings.FPC_EMAIL
+        content = f"Financial Year Report for {'TODO'}"
+
+        context = {
+            "content": content,
+            "user_email": user_email,
+            "external_email": False,
+            "to_email": to_email,
+        }
+
+        body = render_to_string(template, context=context)
+
+        message = EmailMessage(
+            subject=subject,
+            body=body,
+            from_email=settings.FROM_EMAIL,
+            to=to_email,
+        )
+        for attachment in attachments:
+            message.attach(attachment)
+        message.content_subtype = "html"
+        ret = message.send()
+
+        logger.info(
+            f"Email sent to {to_email} with subject '{content}' and return code {ret}"
+        )
